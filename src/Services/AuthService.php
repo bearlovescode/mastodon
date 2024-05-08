830ba76ecc997a50bb1c79c0ca32b358aca67267
Nic Barr <code@nicholasbarr.net>
2024-05-08 10:18:21 -0500

    build authorize url

--- src/Services/AuthService.php
+++ src/Services/AuthService.php
@@ -5,9 +5,23 @@
     use Bearlovescode\Mastodon\Models\Token;
     use DateTime;
     use GuzzleHttp\Psr7\Request;
+    use League\Uri\Uri;
 
     class AuthService extends ApiService
     {
+
+        public function buildAuthorizeUrl(): Uri {
+            $params = [
+                'client_id' => $this->config->clientId,
+                'scope' => 'read+write+push',
+                'redirect_uri' => $this->config->redirect,
+                'response_type' => 'code'
+            ];
+
+            $uriString = sprintf('https://%s?%s', $this->config->instance, http_build_query($params));
+
+            return Uri::new($uriString);
+        }
 
         public function authorize() {
             $req = new Request('GET', '/oauth/authorize', [
