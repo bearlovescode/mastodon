51250983ef2ff31ee6a336ec4e6829fc58740c80
Nic Barr <code@nicholasbarr.net>
2024-06-02 09:06:20 -0500

    working through the redirect_uri process in code.

--- src/Services/InstanceService.php
+++ src/Services/InstanceService.php
@@ -11,7 +11,7 @@
             $req = new Request('POST', '/api/v1/apps', [
                 'form_params' => [
                     'client_name' => $this->config->getAppName(),
-                    'redirect_uris' => $this->config->redirect,
+                    'redirect_uris' => $this->config->getRedirectUrl(),
                     'scopes' => $this->config->scopes,
                     'website' => $this->config->website
                 ]
